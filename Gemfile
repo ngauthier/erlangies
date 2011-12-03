73f720e0d51032a9d37d7ca7944c3d5ccaf9384e
Nick Gauthier <ngauthier@gmail.com>
2011-12-03 15:28:31 -0500

    nuke everything prepping for erlang re-write

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'http://rubygems.org'
-
-gem 'rake'
-gem 'guard'
-gem 'guard-bundler'
-gem 'guard-shell'
-gem 'rb-inotify'
-gem 'libnotify'
--- main.erl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env escript
-%% -*- erlang -*-
-%%! -smp enable -sname useless -mnesia debug verbose
--import(tests).
-main(_) ->
-  tests:run(),
-  io:format("Tests Completed Successfully~n"),
-  halt(0).
